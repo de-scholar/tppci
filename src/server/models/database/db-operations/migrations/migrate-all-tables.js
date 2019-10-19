1295b585011c3e82749370695a9bf445909b9ef6
de_scholar <descholar.stech@gmail.com>
2019-10-19 22:01:09 +0200

    adding npm migrate

--- src/server/models/database/db-operations/migrations/migrate-all-tables.js
+++ src/server/models/database/db-operations/migrations/migrate-all-tables.js
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv';
 import connect from '../../configs/connectToDb';
 import { CREATE_TABLE_APPLICATIONS, CREATE_TABLE_USERS, CREATE_TABLE_WELCOME_MSGS } from '../../configs/SQLqueries';
+
+dotenv.config();
 
 const migrateAllTables = async (isDone) => {
   console.log('creating table applications_for_membership ... ');
